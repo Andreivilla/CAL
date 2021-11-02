a104e5da71b354f06ccb600ff4479bbaa04ad517
AndreiVIlla <andreivilla63@gmaIL.com>
2021-11-02 17:19:38 -0300

    6 sem pilha

--- lista1/ex1/interativo.c
+++ lista1/ex1/interativo.c
@@ -23,14 +23,14 @@ int fibI(int n){
 int main(int argc, char const *argv[]){
     double tempo=0, vetTempo[100];
     int i = 0;
-    while(i < 100){
+    while(i < 46){
         printf("\nn: %d\n", i);
         clock_t t = clock();
         printf("fibo: %d\n",fibI(i));
         t = clock() - t;
         tempo = ((double)t)/((CLOCKS_PER_SEC/1000));
         printf("Tempo de execucao: %lf\n", tempo);
-        //vetTempo[i] = tempo;
+        vetTempo[i] = tempo;
         i++;
     }
 }
--- lista1/ex1/recursivo.c
+++ lista1/ex1/recursivo.c
@@ -35,6 +35,6 @@ int main(int argc, char const *argv[]){
             printf("[%d, ", j);
         printf("%d, ", j);
         if(j==i-1)
-            printf("%d", j);
+            printf("%d]", j);
     }
 }
